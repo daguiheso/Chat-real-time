a8039252028841bbce8c9b6065dc5971c8fa608d
Daniel <danielhernandez16@hotmail.com>
2015-05-18 01:14:16 -0500

    Add course for route management

--- router/index.js
+++ router/index.js
@@ -1,11 +1,16 @@
 const path = require('path')
+const course = require('course')  // manejo de rutas dinamicas
 const st = require('st')  // modulo para servir archivos estaticos
 
+// crear un enrutador 
+const router = course()
 const mount = st({
 	// carpeta definida como estatica - relativa a la carpeta actual, un directorio mas arriba y el folder public
 	path: path.join(__dirname, '..', 'public'),
 	// archivo principal
 	index: 'index.html'
+	// parametro de config 
+	passthrough: true 
 })
 
 /* Montando todo el directorio public en el servidor*/ 
@@ -13,8 +18,12 @@ function onRequest (req, res) {
 	mount(req, res, function (err) {
 		if(err) return 	res.end(err.message)
 
-		res.statusCode = 404
-		res.end(`404 Not found: ${req.url}`)
+		// despues de la ejecucuion de logica de mount(server-static) llamamos al enrutador
+		router(req, res, function (err) {
+			if (err) return fail(err, res)
+			res.statusCode = 404
+			res.end(`404 Not found: ${req.url}`)
+		})
 	})
 }
 
